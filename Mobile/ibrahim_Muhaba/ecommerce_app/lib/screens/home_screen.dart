10ce274a41af97b7779b7c49b0b32381b56af7f6
Phi161l <ibrahimnaik91@gmail.com>
2025-07-29 08:35:30 +0300

    chore: adding flutter_lint and set up custom linter rules in analysis_options.yaml

--- Mobile/ibrahim_Muhaba/ecommerce_app/lib/screens/home_screen.dart
+++ Mobile/ibrahim_Muhaba/ecommerce_app/lib/screens/home_screen.dart
@@ -48,7 +48,7 @@ class HomeScreen extends StatelessWidget {
                         color: Colors.grey,
                       ),
                     ),
-                  ),
+                  )
 
                 ],
               ),
